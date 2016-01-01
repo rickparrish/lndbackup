623a9855536f893cc4facf0d7344c8f4d6a35493
rickparrish <github@rick.parrish>
2016-01-01 11:45:12 -0500

    Add retries to snapshot and replication since 'killed' has happened a few times now

--- lndbackup/Program.cs
+++ lndbackup/Program.cs
@@ -33,6 +33,7 @@ namespace lndbackup
                         }
                         else
                         {
+                            Console.WriteLine($"Found {VMIDsToBackup.Count} VMs to backup");
                             foreach (int VMID in VMIDsToBackup)
                             {
                                 try
@@ -70,39 +71,45 @@ namespace lndbackup
             }
             else
             {
-                Console.WriteLine("USAGE: lndbackup <api_id> <api_key> <source> <destination>");
-                Console.WriteLine();
-                Console.WriteLine("     api_id           API ID from lndynamic control panel");
-                Console.WriteLine("     api_key          API Key from lndynamic control panel");
-                Console.WriteLine("     source           vm_id or region name to backup");
-                Console.WriteLine("     destination      region name to replicate snapshots to");
-                Console.WriteLine();
-                Console.WriteLine("EXAMPLES");
-                Console.WriteLine();
-                Console.WriteLine("     lndbackup api_id api_key 12345 toronto");
-                Console.WriteLine("     Backup VM with id 12345 and replicate the snapshot to Toronto");
-                Console.WriteLine("     (If the VM is provisioned in Toronto, no replication occurs)");
-                Console.WriteLine();
-                Console.WriteLine("     lndbackup api_id api_key toronto roubaix");
-                Console.WriteLine("     Backup all VMs in Toronto and replicate the snapshots to Roubaix");
-                Console.WriteLine();
-                Console.WriteLine("     lndbackup api_id api_key montreal montreal");
-                Console.WriteLine("     Backup all VMs in Montreal, and don't replicate the snapshots");
-                Console.WriteLine();
-                Console.WriteLine("VM_IDs");
-                Console.WriteLine();
-                Console.WriteLine("     You can find the vm_id for a VM by loading it from the lndynamic control");
-                Console.WriteLine("     panel and looking at the querystring.  For example, this is vm_id 12345:");
-                Console.WriteLine("     https://dynamic.lunanode.com/panel/vm.php?vm_id=12345");
-                Console.WriteLine();
-                Console.WriteLine("REGIONS");
-                Console.WriteLine();
-                Console.WriteLine("     Currently available regions are toronto, montreal, and roubaix");
+                DisplayUsage();
             }
 
+            Console.WriteLine();
             Console.WriteLine("done");
 
             if (Debugger.IsAttached) Console.ReadKey();
+        }
+
+        private static void DisplayUsage()
+        {
+            Console.WriteLine("USAGE: lndbackup <api_id> <api_key> <source> <destination>");
+            Console.WriteLine();
+            Console.WriteLine("     api_id           API ID from lndynamic control panel");
+            Console.WriteLine("     api_key          API Key from lndynamic control panel");
+            Console.WriteLine("     source           vm_id or region name to backup");
+            Console.WriteLine("     destination      region name to replicate snapshots to");
+            Console.WriteLine();
+            Console.WriteLine("EXAMPLES");
+            Console.WriteLine();
+            Console.WriteLine("     lndbackup api_id api_key 12345 toronto");
+            Console.WriteLine("     Backup VM with id 12345 and replicate the snapshot to Toronto");
+            Console.WriteLine("     (If the VM is provisioned in Toronto, no replication occurs)");
+            Console.WriteLine();
+            Console.WriteLine("     lndbackup api_id api_key toronto roubaix");
+            Console.WriteLine("     Backup all VMs in Toronto and replicate the snapshots to Roubaix");
+            Console.WriteLine();
+            Console.WriteLine("     lndbackup api_id api_key montreal montreal");
+            Console.WriteLine("     Backup all VMs in Montreal, and don't replicate the snapshots");
+            Console.WriteLine();
+            Console.WriteLine("VM_IDs");
+            Console.WriteLine();
+            Console.WriteLine("     You can find the vm_id for a VM by loading it from the lndynamic control");
+            Console.WriteLine("     panel and looking at the querystring.  For example, this is vm_id 12345:");
+            Console.WriteLine("     https://dynamic.lunanode.com/panel/vm.php?vm_id=12345");
+            Console.WriteLine();
+            Console.WriteLine("REGIONS");
+            Console.WriteLine();
+            Console.WriteLine("     Currently available regions are toronto, montreal, and roubaix");
         }
 
         private static async Task<List<int>> GetVMIDsToBackup(LNDynamic client, string source)
@@ -135,24 +142,53 @@ namespace lndbackup
             Console.WriteLine($"  - region  : {Details.extra.region}");
 
             // Take a snapshot of the VM
+            int SnapshotRetries = 0;
+            RetrySnapshot:
             Console.WriteLine($"  - Creating snapshot image...");
             string NewImageName = $"lndbackup {vmId} {DateTime.Now.ToString("yyyy-MM-dd")} {Details.extra.hostname}";
             int NewImageId = await client.VMSnapshotAsync(vmId, NewImageName);
             Console.WriteLine($"    - New snapshot image {NewImageId} queued for creation!");
 
             // Wait for new image to be 'active'
-            WaitForImageToBecomeActive(client, NewImageId);
+            try {
+                await WaitForImageToBecomeActive(client, NewImageId);
+            } catch (LNDImageKilledException) {
+                await client.ImageDeleteAsync(NewImageId);
+                if (SnapshotRetries++ < 3)
+                {
+                    Console.WriteLine($"    - Creation failed, waiting 30 seconds for retry #{SnapshotRetries}/3");
+                    Thread.Sleep(30000);
+                    goto RetrySnapshot;
+                }
+                throw;
+            }
 
             // Replicate image to new region (if necessary)
             if (Details.extra.region != destinationRegion)
             {
                 // Replicate the image to the new region
-                Console.WriteLine($"  - Replicating snapshot image...");
+                int ReplicateRetries = 0;
+                RetryReplicate:
+                Console.WriteLine($"  - Replicating snapshot image to {destinationRegion}...");
                 int ReplicatedImageId = await client.ImageReplicateAsync(NewImageId, destinationRegion);
                 Console.WriteLine($"    - New snapshot image {ReplicatedImageId} queued for replication!");
 
                 // Wait for new image to be 'active'
-                WaitForImageToBecomeActive(client, ReplicatedImageId);
+                try
+                {
+                    await WaitForImageToBecomeActive(client, ReplicatedImageId);
+                }
+                catch (LNDImageKilledException)
+                {
+                    await client.ImageDeleteAsync(ReplicatedImageId);
+                    if (ReplicateRetries++ < 3)
+                    {
+                        Console.WriteLine($"    - Creation failed, waiting 30 seconds for retry #{ReplicateRetries}/3");
+                        Thread.Sleep(30000);
+                        goto RetryReplicate;
+                    }
+                    throw;
+                }
 
                 // Delete original image, leaving only replicated image
                 Console.WriteLine($"  - Removing original snapshot...");
@@ -175,12 +211,9 @@ namespace lndbackup
             Console.WriteLine("    - Image downloaded successfully!");
 
             // TODO Use client.ImageList to find old lndbackup generated images for this VM and delete them
-
-            Console.WriteLine();
-            Console.WriteLine();
         }
 
-        private static async void WaitForImageToBecomeActive(LNDynamic client, int newImageId)
+        private static async Task WaitForImageToBecomeActive(LNDynamic client, int newImageId)
         {
             string NewImageStatus = (await client.ImageDetailsAsync(newImageId)).status;
             while (NewImageStatus != "active")
@@ -188,6 +221,8 @@ namespace lndbackup
                 // TODO One one run status was 'killed'.  Do we abort, or delete and retry replication, or ?
                 //      Maybe instead of looking for 'killed', look for NOT 'queued' or 'saving'?
                 //      There's the snapshot watch above too (maybe this duplicate logic should be extracted to its own method)
+                if (NewImageStatus == "killed") throw new LNDImageKilledException("image status=killed");
+
                 Console.WriteLine($"      - Image status is '{NewImageStatus}', waiting 30 seconds for 'active'...");
                 Thread.Sleep(30000);
                 NewImageStatus = (await client.ImageDetailsAsync(newImageId)).status;
